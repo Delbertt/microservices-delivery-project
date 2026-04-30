9c3a593d8acd7191b10302485273ae918ac95351
Delbertt <a.elbertt@hotmail.com>
2026-04-29 22:20:41 -0300

    add orm mapping for the courier domain and entities

--- Microservices/Courier-Management/src/main/java/com/arthlnx/deliveryproj/courier/management/domain/model/Courier.java
+++ Microservices/Courier-Management/src/main/java/com/arthlnx/deliveryproj/courier/management/domain/model/Courier.java
@@ -1,5 +1,9 @@
 package com.arthlnx.deliveryproj.courier.management.domain.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.*;
 import org.hibernate.validator.cfg.defs.UUIDDef;
 
@@ -9,13 +13,14 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-
+@Entity
 @Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Setter(AccessLevel.PRIVATE)
 public class Courier {
 
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
 
@@ -31,6 +36,7 @@ public class Courier {
 
     private OffsetDateTime lastFulfilledDeliveryAt;
 
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "courier")
     private List<AssignedDelivery> pendingDeliveries = new ArrayList<>();
 
     public List<AssignedDelivery> getPendingDeliveries() {
@@ -50,7 +56,7 @@ public class Courier {
 
     public void assign(UUID deliveryID) {
         this.pendingDeliveries.add(
-                AssignedDelivery.pending(deliveryID)
+                AssignedDelivery.pending(deliveryID, this)
         );
         this.pendingDeliveriesQuantity++;
     }
