da6ea8ef22859070ceb5f594a46b64aec3fab569
Delbertt <a.elbertt@hotmail.com>
2026-06-27 19:42:44 -0300

    add eureka service register

---
+++ Microservices/Courier-Management/src/main/java/com/arthlnx/deliveryproj/courier/management/infrastructure/Eureka/EurekaConfig.java
@@ -0,0 +1,9 @@
+package com.arthlnx.deliveryproj.courier.management.infrastructure.Eureka;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class EurekaConfig {
+}
---
+++ Microservices/Delivery-Tracking/src/main/java/com/arthlnx/deliveryproj/delivery/tracking/infrastructure/eureka/EurekaConfig.java
@@ -0,0 +1,9 @@
+package com.arthlnx.deliveryproj.delivery.tracking.infrastructure.eureka;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class EurekaConfig {
+}
