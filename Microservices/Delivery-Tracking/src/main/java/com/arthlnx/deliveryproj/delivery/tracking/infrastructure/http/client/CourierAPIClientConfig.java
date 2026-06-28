8398e7e189fff02c70107878aaf324c11d311b3f
Delbertt <a.elbertt@hotmail.com>
2026-06-27 22:09:56 -0300

    add bean handler for routing loadbalanced usage

--- Microservices/Delivery-Tracking/src/main/java/com/arthlnx/deliveryproj/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
+++ Microservices/Delivery-Tracking/src/main/java/com/arthlnx/deliveryproj/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
@@ -1,7 +1,10 @@
 package com.arthlnx.deliveryproj.delivery.tracking.infrastructure.http.client;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -10,9 +13,23 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 public class CourierAPIClientConfig {
 
     @Bean
-    public CourierAPIClient courierAPIClient(RestClient.Builder restClientBuilder) {
+    @Primary // Primary bean noted for enabling eureka client safely make a
+    // request to it server without going trought the load balancer
+    public RestClient.Builder plainRestClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean(name = "courierRestClientBuilder")
+    @LoadBalanced
+    public RestClient.Builder loadBalancedRestClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    public CourierAPIClient courierAPIClient(
+            @Qualifier("courierRestClientBuilder") RestClient.Builder restClientBuilder) {
         RestClient restClient = restClientBuilder
-                .baseUrl("http://localhost:8081/")
+                .baseUrl("http://courier-management/")
                 .build();
 
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
